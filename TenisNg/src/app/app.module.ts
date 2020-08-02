8535e2b01e3a6b4b4e5e64b1bb8ac7a9c2b92a1f
MAndrea98 <mendrei.andrea@gmail.com>
2020-08-02 20:41:47 +0200

    Uradjeno izlistavanje agenata

--- TenisNg/src/app/app.module.ts
+++ TenisNg/src/app/app.module.ts
@@ -9,12 +9,14 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
 import { AgentListComponent } from './default-page/agent-list/agent-list.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FormsModule } from "@angular/forms";
+import { NavBarComponent } from './default-page/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DefaultPageComponent,
-    AgentListComponent
+    AgentListComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
--- TenisNg/src/app/default-page/agent-list/agent-list.component.ts
+++ TenisNg/src/app/default-page/agent-list/agent-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { AID } from 'src/app/_models/AID';
 
 @Component({
     selector: 'app-agent-list',
@@ -7,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   })
   
 export class AgentListComponent implements OnInit {
+    
+  router: String;
+  agents: AID[] = [];
 
-    constructor() { }
+  constructor(_router:Router, private http:HttpClient) {
+    this.router=_router.url;
+  }
+
+  public ngOnInit() { 
+    this.getAllAgents();
+  }
   
-    public ngOnInit() { }
-  
+  getAllAgents() {
+    let url = "http://localhost:8080/TenisWAR/rest/agents/running";
+    this.http.get(url).subscribe(
+      (res:AID[])=>{
+        this.agents = res;
+      },
+      err=>{
+        alert('Something went wrong');
+        console.log(err.message);
+      }
+    )
+  }
+
+  stopAgent(agent: AID):void {
+    let url = "http://localhost:8080/TenisWAR/rest/agents/running";
+    this.http.delete(url, { responseType: 'text' }).subscribe(
+      res => {
+        this.getAllAgents();
+      },
+      err => {
+        alert('Something went wrong');
+        console.log(err.message);
+      }
+    )
+  }
     
 }
