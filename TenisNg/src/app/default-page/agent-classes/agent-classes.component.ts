c69bb692d111ae2f1697347239dd374776e2e533
MilenaVujicic <vujicic_milena@uns.ac.rs>
2020-08-06 22:43:00 +0200

    Added agent types list

--- TenisNg/src/app/default-page/agent-classes/agent-classes.component.ts
+++ TenisNg/src/app/default-page/agent-classes/agent-classes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AgentType} from '../../_models/AgentType';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-agent-classes',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgentClassesComponent implements OnInit {
 
-  constructor() { }
+  agentClasses:AgentType[] = [];
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.getAgentClasses();
+  }
+
+  getAgentClasses():void{
+    let url = "http://localhost:8080/TenisWAR/rest/agents/classes";
+    this.http.get(url).subscribe(
+      (res:AgentType[])=>{this.agentClasses=res;},
+      err=>{alert("Something went wrong"); console.log(err.meessage);}
+    );
   }
 
 }
