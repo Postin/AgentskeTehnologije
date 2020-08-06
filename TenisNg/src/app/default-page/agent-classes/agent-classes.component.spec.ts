809d88614a25d43874967bc8e8f7673bc69b99ed
MilenaVujicic <vujicic_milena@uns.ac.rs>
2020-08-06 22:29:45 +0200

    Added performatives list on frontend

---
+++ TenisNg/src/app/default-page/agent-classes/agent-classes.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AgentClassesComponent } from './agent-classes.component';
+
+describe('AgentClassesComponent', () => {
+  let component: AgentClassesComponent;
+  let fixture: ComponentFixture<AgentClassesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AgentClassesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AgentClassesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ TenisNg/src/app/default-page/agent-classes/agent-classes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-classes',
+  templateUrl: './agent-classes.component.html',
+  styleUrls: ['./agent-classes.component.css']
+})
+export class AgentClassesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
