7900cee584c39add77a5e44a147d43b0590e7f08
MAndrea98 <mendrei.andrea@gmail.com>
2020-08-14 13:48:38 +0200

    Promenjen je nacin poziva backenda

--- TenisNg/src/app/default-page/agent-list/agent-list.component.ts
+++ TenisNg/src/app/default-page/agent-list/agent-list.component.ts
@@ -24,13 +24,11 @@ export class AgentListComponent implements OnInit {
   }
 
   public ngOnInit() { 
-    this.getAllAgents();
-
     let call = this;
     this.address = window.location.href.split(":")[1];
     this.address = this.address.substring(2);
     console.log(this.address);
-    this.address = "192.168.56.1"; // ovo zakomentarisati posle
+    // this.address = "192.168.56.1"; // ovo zakomentarisati posle
     this.host = "ws://" + this.address + ":8080/TenisWAR/ws";
     try {
       this.socket = new WebSocket(this.host);
@@ -55,7 +53,8 @@ export class AgentListComponent implements OnInit {
   }
   
   getAllAgents() {
-    let url = "http://192.168.56.1:8080/TenisWAR/rest/agents/running";
+    let url = "http://"+this.address+":8080/TenisWAR/rest/agents/running";
+    console.log(url);
     this.http.get(url).subscribe(
       (res:AID[])=>{
         this.agents = res;
@@ -68,7 +67,7 @@ export class AgentListComponent implements OnInit {
   }
 
   stopAgent(agent: AID):void {
-    let url = "http://192.168.56.1:8080/TenisWAR/rest/agents/running";
+    let url = "http://"+this.address+":8080/TenisWAR/rest/agents/running";
     const options = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
@@ -94,7 +93,7 @@ export class AgentListComponent implements OnInit {
     const options = {
       responseType: 'text' as const,
     }
-    let url = "http://192.168.56.1:8080/TenisWAR/rest/agents/running/" + agentT + "/" + agentN;
+    let url = "http://"+this.address+":8080/TenisWAR/rest/agents/running/" + agentT + "/" + agentN;
     this.http.put(url, {agentT, agentN}, options).subscribe(
       (res:string) => {
         alert(res);
